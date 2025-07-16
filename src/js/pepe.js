9907807a43d619b7345606ee840353dd5ec92c63
Garcilazoae <garcilazo.ae@gmail.com>
2025-07-16 10:07:58 -0300

    Agregar nuevo archivo php.php y modificar index.html y pepe.js

--- src/js/pepe.js
+++ src/js/pepe.js
@@ -1,1 +1,36 @@
-window.onload(console.log("Entró!!!"));
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("formulario");
+    const edadInput = document.getElementById("edad");
+    const mensajeError = document.getElementById("mensajeError");
+    const mensajeExito = document.getElementById("mensajeExito");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const edad = parseInt(edadInput.value);
+        if (edad < 16) {
+        mensajeError.classList.remove("d-none");
+        mensajeExito.classList.add("d-none");
+        return;
+    }
+
+      // Enviar con fetch a PHP
+    const formData = new FormData(form);
+    fetch("./php/procesar.php", {
+        method: "POST",
+        body: formData,
+    })
+        .then((res) => res.text())
+        .then((data) => {
+            if (data === "ok") {
+            mensajeError.classList.add("d-none");
+            mensajeExito.classList.remove("d-none");
+            form.reset();
+        } else {
+            alert("Error al enviar inscripción: " + data);
+        }
+        })
+        .catch((err) => {
+            alert("Error de conexión");
+        });
+    });
+});
